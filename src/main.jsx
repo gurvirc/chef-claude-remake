d3a55c35b7b1a6c7a4f247be34e071c37d6c2879
gurvirc <gca83@sfu.ca>
2025-05-11 20:48:41 -0700

    Added state to manage user-inputted ingredients and display them dynamically.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
 import React from "react"
 export default function Main() {
+    
+    const [ingredients, setIngredients]=React.useState([])
+
+    const listOfIngredients= ingredients.map(ingredient=> (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function addIngredient(formData){
+        const newIngredient=formData.get("ingredient")
+        setIngredients(prev=> [...prev, newIngredient])
+    }
+
     return(
         <main>
-        <form className="input-form-ingredients">
-            <input type="text" name="ingredients" placeholder="   eg.oregano"></input>
+        <form action={addIngredient} className="input-form-ingredients">
+            <input type="text" name="ingredient" placeholder="   eg.oregano"></input>
             <button>+ Add Ingredient</button>
         </form>
         <section className="Ingredients-list-items">
             <h2>Ingredients on hand:</h2>
+            <ul>{listOfIngredients}</ul>
         </section>
         </main>
     )
