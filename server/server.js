1e946e8a70b1d851aba7b07216ee4ac55d2f627a
gurvirc <gca83@sfu.ca>
2026-01-14 12:04:48 -0800

    Wire signup form to /api/auth/register

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import cors from 'cors'
+import { authRouter } from './routes/auth.js'
 
 const app= express()
 const PORT= 3000
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
+app.use(express.json())//this is required to be able to access req.body params
+
+app.use('/api/auth', authRouter)
 
 
 
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,25 +1,57 @@
 import ChefClaudeLogo from './images/ChefClaudeIcon.png'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom' 
 export default function Signup(){
+
+    const navigate= useNavigate()
+    async function signUp(formData){
+        const name= formData.get("name")
+        const email= formData.get("email")
+        const username= formData.get("username")
+        const password= formData.get("password")
+        
+       try{
+            const res= await fetch('http://localhost:3000/api/auth/register' ,{
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({name, email, username, password})
+            }) 
+                const data= await res.json()
+
+                if(res.ok){
+                    navigate('/')
+                }/*add else statement to handle a registration error*/
+                
+
+            }catch(err){
+                console.log('Network error ', err)
+            }
+        
+    }
+
+
+
     return(
         <div className="login-page">
-            <form className="login-form" action="">
+            <form className="login-form" action={signUp}>
                 <img className="signup-logo" src={ChefClaudeLogo}></img>
                 <h1>Sign Up</h1>
                 <div className="input-box">
-                    <input type="name" placeholder="Full Name" required/>
+                    <input type="name" name="name" placeholder="Full Name" required/>
                 </div>
                 <div className="input-box">
-                    <input type="text" placeholder="Email Address" required/>
+                    <input type="text" name="email" placeholder="Email Address" required/>
                 </div>
                 <div className="input-box">
-                    <input type="username" placeholder="Username" required/>
+                    <input type="username" name="username" placeholder="Username" required/>
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder="Password" required/>
+                    <input type="password" name="password" placeholder="Password" required/>
                 </div>
                 
-                <button type="submit">Create Account</button>
+                <button /*onClick={handleRegister}*/ type="submit">Create Account</button>
                 <p>Already registered?<Link className="Login-link" to="/Login">Sign in here</Link></p>
 
             </form>
